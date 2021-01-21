19196c1ec125b4a5b2c4e511683c21470e27c888
oliver-hull-iii <ohull1@binghamton.edu>
2021-01-20 21:52:44 -0500

    no unused vars

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {Container, Col, Row, Button, Card } from 'react-bootstrap';
 import './css/home.css';
 import Gallery from 'react-photo-gallery';
