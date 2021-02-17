59d2f293e798245154870aad74d9b677e42222cf
oliver-hull-iii <ohull1@binghamton.edu>
2021-02-16 19:55:49 -0500

    redirects

--- src/AnimalCare.js
+++ src/AnimalCare.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from 'react'; 
+import { Col, Container, Row } from 'react-bootstrap';
 
-function AnimalCare(props) {
-    return(
-        <h1>Animal Care</h1>        
-    );
-}
-  
+
+const AnimalCare = () => (
+
+    <Container fluid className="secondary-color" id="animalcare">
+
+        <Container className="p-1 p-md-2">
+            <Row>
+                <Col>
+                <h1 className="m-3">Animal Care</h1>
+                </Col> 
+            </Row>
+            <Row  className="mb-4"> 
+            </Row>   
+        </Container>
+
+
+    </Container>
+);
+
+
 export default AnimalCare;
